5f41bdb24a616c912f61601a279af029af292cd0
Dima <dimitriy@ukr.net>
2014-03-19 21:39:14 +0200

    Finish pages and Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MyCat1::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/about"
-  get "static_pages/contacts"
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contacts', to: 'static_pages#contacts'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
